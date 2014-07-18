9102715eb40e13fc16eee585fddacb2429369f3e
bennypi <bennypi@tzi.de>
2014-07-18 22:44:13 +0200

    documented the ContactPlugin.cc and ContactPlugin.hh

--- include/ContactPlugin.hh
+++ include/ContactPlugin.hh
@@ -12,32 +12,40 @@
 /// Thanks to the gazebo Tutorial.
 namespace gazebo
 {
-  /// \brief An example plugin for a contact sensor.
-  class ContactPlugin : public SensorPlugin
-  {
-    /// \brief Constructor.
-    public: ContactPlugin();
+/// \brief Class to check for Contacts with the suction cup
+///
+/// Every time the OnUpdate() method is called, all the contacts
+/// are evaluated. If there is a contact between the suction cup
+/// and an other object in the world, the modelname and linkname
+/// is send to the suction cup controller.
+class ContactPlugin : public SensorPlugin
+{
+/// \brief Constructor.
+public: ContactPlugin();
 
-    /// \brief Destructor.
-    public: virtual ~ContactPlugin();
+/// \brief Destructor.
+public: virtual ~ContactPlugin();
 
-    /// \brief Load the sensor plugin.
-    /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
-    /// \param[in] _sdf SDF element that describes the plugin.
-    public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+/// \brief Load the sensor plugin.
+/// \param[in] _sensor Pointer to the sensor that loaded this plugin.
+/// \param[in] _sdf SDF element that describes the plugin.
+public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
 
-    /// \brief Callback that recieves the contact sensor's update signal.
-    private: virtual void OnUpdate();
+/// \brief Callback that recieves the contact sensor's update signal.
+private: virtual void OnUpdate();
 
-    /// \brief Pointer to the contact sensor
-    private: sensors::ContactSensorPtr parentSensor;
+/// \brief Pointer to the contact sensor
+private: sensors::ContactSensorPtr parentSensor;
 
-    /// \brief Connection that maintains a link between the contact sensor's
-    /// updated signal and the OnUpdate callback.
-    private: event::ConnectionPtr updateConnection;
+/// \brief Connection that maintains a link between the contact sensor's
+/// updated signal and the OnUpdate callback.
+private: event::ConnectionPtr updateConnection;
 
-    private: gazebo::transport::PublisherPtr pub;
-    public: void send_message(const char *contact_name);
-  };
+/// publisher for the contacts
+private: gazebo::transport::PublisherPtr pub;
+
+/// method to send the model's and link's names
+public: void send_message(const char *contact_name);
+};
 }
 #endif
--- src/ContactPlugin.cc
+++ src/ContactPlugin.cc
@@ -4,77 +4,80 @@ using namespace gazebo;
 GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
 
 /////////////////////////////////////////////////
-ContactPlugin::ContactPlugin() :
-		SensorPlugin() {
-//	gazebo::load();
-//	gazebo::transport::init();
-//	gazebo::transport::run();
-	gazebo::transport::NodePtr node(new gazebo::transport::Node());
+ContactPlugin::ContactPlugin()
+    : SensorPlugin()
+{
+  // create node fur publisher
+  gazebo::transport::NodePtr node(new gazebo::transport::Node());
+  node->Init("suction_cup_contact_publisher");
 
-	node->Init("suction_cup_contact_publisher");
-	this->pub = node->Advertise<uarm_msgs::msgs::SuctionCupContact>(
-			"/uarm/suction_cup_contact"); //add topic name
-
-//	std::cout << "Waiting for connection." << std::endl;
-//	pub->WaitForConnection();
-//	std::cout << "Connection established." << std::endl;
+  // create publisher to send contacts
+  this->pub = node->Advertise<uarm_msgs::msgs::SuctionCupContact>(
+      "/uarm/suction_cup_contact");
 }
 
 /////////////////////////////////////////////////
-ContactPlugin::~ContactPlugin() {
-	gazebo::transport::fini();
+ContactPlugin::~ContactPlugin()
+{
+  // end transport
+  gazebo::transport::fini();
 }
 
 /////////////////////////////////////////////////
-void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/) {
+void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  // get the parent sensor
+  this->parentSensor = boost::dynamic_pointer_cast<sensors::ContactSensor>(
+      _sensor);
 
-	// Get the parent sensor.
-	this->parentSensor = boost::dynamic_pointer_cast<sensors::ContactSensor>(
-			_sensor);
+  // make sure the parent sensor is valid
+  if (!this->parentSensor)
+  {
+    gzerr << "ContactPlugin requires a ContactSensor.\n";
+    return;
+  }
 
-	// Make sure the parent sensor is valid.
-	if (!this->parentSensor) {
-		gzerr << "ContactPlugin requires a ContactSensor.\n";
-		return;
-	}
+  // connect to the sensor update event
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      boost::bind(&ContactPlugin::OnUpdate, this));
 
-	// Connect to the sensor update event.
-	this->updateConnection = this->parentSensor->ConnectUpdated(
-			boost::bind(&ContactPlugin::OnUpdate, this));
-
-	// Make sure the parent sensor is active.
-	this->parentSensor->SetActive(true);
-
+  // make sure the parent sensor is active
+  this->parentSensor->SetActive(true);
 }
 
 /////////////////////////////////////////////////
-void ContactPlugin::OnUpdate() {
-	// Get all the contacts.
-	msgs::Contacts contacts;
-	contacts = this->parentSensor->GetContacts();
-	for (unsigned int i = 0; i < contacts.contact_size(); ++i) {
-//    if(contacts.contact(i).collision1() != "ground_plane::ground_link::collision" &&
-//    		contacts.contact(i).collision2() != "ground_plane::ground_link::collision"){
-//    	std::cout << "Collision between[" << contacts.contact(i).collision1()
-//    	              << "] and [" << contacts.contact(i).collision2() << "]\n";
-//    }
+void ContactPlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->GetContacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+  {
+    // make sure that one collision is from the suction cup
+    // and the other collision is NOT from the suction cup
+    if (std::strcmp(contacts.contact(i).collision1().c_str(),
+                    "uarm::suction_cup::collision") == 0
+        && std::strcmp(contacts.contact(i).collision2().c_str(),
+                       "uarm::suction_cup::collision") != 0)
+      // send the collision that is NOT from the suction cup
+      this->send_message(contacts.contact(i).collision2().c_str());
 
-//    if ((contacts.contact(i).collision1() == "uarm::suction_cup::collision" && contacts.contact(i).collision2() == "uarm_box::link::uarm_box_collision")
-//        			|| (contacts.contact(i).collision2() == "uarm::suction_cup::collision" && contacts.contact(i).collision1() == "uarm_box::link::uarm_box_collision")){
-//    	std::cout << "Found contact between suction cup and uarm_box\n";
-		if (std::strcmp(contacts.contact(i).collision1().c_str(), "uarm::suction_cup::collision")== 0 && std::strcmp(contacts.contact(i).collision2().c_str(), "uarm::suction_cup::collision")!= 0)
-			this->send_message(contacts.contact(i).collision2().c_str());
-		if (std::strcmp(contacts.contact(i).collision2().c_str(), "uarm::suction_cup::collision")== 0 && std::strcmp(contacts.contact(i).collision1().c_str(), "uarm::suction_cup::collision")!= 0)
-			this->send_message(contacts.contact(i).collision1().c_str());
-//    }
-	}
+    // make sure that one collision is from the suction cup
+    // and the other collision is NOT from the suction cup
+    if (std::strcmp(contacts.contact(i).collision2().c_str(),
+                    "uarm::suction_cup::collision") == 0
+        && std::strcmp(contacts.contact(i).collision1().c_str(),
+                       "uarm::suction_cup::collision") != 0)
+      // send the collision that is NOT from the suction cup
+      this->send_message(contacts.contact(i).collision1().c_str());
+  }
 }
 
-void ContactPlugin::send_message(const char* contact_name) {
-	uarm_msgs::msgs::SuctionCupContact msg;
-	//uarm_msgs::msgs::NewJointPosition joint_msg;
-	msg.set_collision_name(contact_name);
-//	std::cout << "Trying to send message." << std::endl;
-	pub->Publish(msg);
-//	std::cout << "Send message." << std::endl;
+/////////////////////////////////////////////////
+void ContactPlugin::send_message(const char* contact_name)
+{
+  // create the message, add the contact_name and publish it
+  uarm_msgs::msgs::SuctionCupContact msg;
+  msg.set_collision_name(contact_name);
+  pub->Publish(msg);
 }
